7d232b174a83338cbb25e37dfa48f1e6dee0710a
Ashish Mishra <ashishgk1999@gmail.com>
2024-02-19 23:05:58 +0530

    editing navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
-import Header from "./components/Header";
 import Contact from "./pages/Contact";
 import Navbar from "./pages/Navbar";
 
 function App() {
   return (
     <Router>
-      {/* <Header /> */}
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
